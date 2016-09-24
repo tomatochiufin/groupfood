0962449bf6dfadc7a27eaa268a3903fd787bc209
Stacy Chen <chiufin@gmail.com>
2016-09-24 19:32:25 +0800

    show groups list

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
+    <div v-for="group in groups">
+        <h2>{{ group.restaurant }}</h2>
+        <ul>
+            <li v-for="person in group.participant">{{person.name}}</li>
+        </ul>
+
+    </div>
+
   </div>
 </template>
 
@@ -9,7 +17,21 @@
 export default {
   data () {
     return {
-      msg: 'Hello Vue!'
+      msg: 'Group Food!',
+      groups: [
+        {
+          restaurant: '添好運',
+          participant: [{name: 'stacy chen'}, {name: 'yingray lu'}, {name: 'pompom huang'}]
+        },
+        {
+          restaurant: '點點心',
+          participant: [{name: 'liz cheng'}, {name: 'Gary Chung'}, {name: 'Amber ou'}]
+        },
+        {
+          restaurant: 'UnGroup',
+          participant: [{name: 'Jerry Chen'}, {name: 'Nicky Chen'}, {name: 'Mike chen'}]
+        },
+      ]
     }
   }
 }
