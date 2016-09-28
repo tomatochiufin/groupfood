fbd54e6d3a08fe4c3e250f5d810e135f9ddcb1fa
Stacy Chen <chiufin@gmail.com>
2016-09-29 06:52:07 +0800

    vue components display with data

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,10 +1,44 @@
 <template>
-  <div v-on:click="test">Hello World!</div>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div v-for="group in groups">
+      <h2>{{ group.restaurant }}</h2>
+      <ul>
+        <li v-for="person in group.participant">{{person.name}}</li>
+      </ul>
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default{
   name: 'Group',
+  data:()=>{
+    return {
+          msg: 'Group Food!',
+          hello: {
+            world: "how are you"
+          },
+          groups: [
+            {
+              restaurant: '添好運',
+              participant: [{name: 'stacy chen'}, {name: 'yingray lu'}, {name: 'pompom huang'}],
+              remain: 0
+            },
+            {
+              restaurant: '點點心',
+              participant: [{name: 'liz cheng'}, {name: 'Gary Chung'}, {name: 'Amber ou'}],
+              remain: 1
+            },
+            {
+              restaurant: '花酒藏',
+              participant: [{name: 'Jerry Chen'}, {name: 'Nicky Chen'}, {name: 'Mike chen'}],
+              remain: 2
+            },
+          ],
+    }
+  },
   methods: {
     test:function(){
              console.log("hello world");
--- src/components/UnGroup.vue
+++ src/components/UnGroup.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>UnGroup</div>
+  <div>
+    <div v-for="person in ungroup">
+        <p>{{person.name}}</p>
+        <select>
+          <option v-for="group in groups">
+            {{group.restaurant}}
+          </option>
+        </select>
+        <button type="button">入團</button>
+      </div>
+  </div>
 </template>
 
 <script>
 export default{
   name: 'UnGroup',
-
+  data(){
+    return {
+        msg: 'Group Food!',
+        groups: [
+          {
+            restaurant: '添好運',
+            participant: [{name: 'stacy chen'}, {name: 'yingray lu'}, {name: 'pompom huang'}],
+            remain: 0
+          },
+          {
+            restaurant: '點點心',
+            participant: [{name: 'liz cheng'}, {name: 'Gary Chung'}, {name: 'Amber ou'}],
+            remain: 1
+          },
+          {
+            restaurant: '花酒藏',
+            participant: [{name: 'Jerry Chen'}, {name: 'Nicky Chen'}, {name: 'Mike chen'}],
+            remain: 2
+          },
+        ],
+        ungroup: [
+          {
+            name: 'Kiki Huang',
+            status: 'no_group'
+          },
+          {
+            name: 'Javan',
+            status: 'leave'
+          }
+        ]
+      }
+  }
 
 }
 
